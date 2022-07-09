03911f642de13881d92318235a3d227f9b011aa8
Paweł Sowa <pawel@the-sowa.com>
2022-07-09 09:41:43 +0200

    feat: add philips briliance config

--- autostart.sh
+++ autostart.sh
@@ -8,6 +8,8 @@ conky -c $HOME/.config/conky/doomone-qtile.conkyrc
 volumeicon &
 nm-applet &
 
+# xrandr --output eDP-1 --mode 1920x1080 --pos 5120x180 --rotate normal --output DP-1 --off --output DP-2 --primary --mode 5120x1440_55.00 --pos 0x0 --rotate normal --output DP-3 --off
+
 ### UNCOMMENT ONLY ONE OF THE FOLLOWING THREE OPTIONS! ###
 # 1. Uncomment to restore last saved wallpaper
 # xargs xwallpaper --stretch < ~/.xwallpaper &
