8dc220e9be08217497fcda1508722f4089b83e8f
Paweł Sowa <pawel@the-sowa.com>
2022-07-22 17:09:12 +0200

    refactor: unify widgets on every screen

--- config.py
+++ config.py
@@ -299,30 +299,15 @@ def init_widgets_list():
     return widgets_list
 
 
-def init_widgets_screen1():
-    widgets_screen1 = init_widgets_list()
-    # Slicing removes unwanted widgets (systray) on Monitors 1,3
-    del widgets_screen1[9:10]
-    return widgets_screen1
-
-
-def init_widgets_screen2():
-    widgets_screen2 = init_widgets_list()
-    # Monitor 2 will display all widgets in widgets_list
-    return widgets_screen2
-
-
 def init_screens():
-    return [Screen(top=bar.Bar(widgets=init_widgets_screen1(), opacity=1.0, size=20)),
-            Screen(top=bar.Bar(widgets=init_widgets_screen2(), opacity=1.0, size=20)),
-            Screen(top=bar.Bar(widgets=init_widgets_screen1(), opacity=1.0, size=20))]
+    return [Screen(top=bar.Bar(widgets=init_widgets_list(), opacity=1.0, size=20)),
+            Screen(top=bar.Bar(widgets=init_widgets_list(), opacity=1.0, size=20)),
+            Screen(top=bar.Bar(widgets=init_widgets_list(), opacity=1.0, size=20))]
 
 
 if __name__ in ["config", "__main__"]:
     screens = init_screens()
     widgets_list = init_widgets_list()
-    widgets_screen1 = init_widgets_screen1()
-    widgets_screen2 = init_widgets_screen2()
 
 
 def window_to_prev_group(qtile):
