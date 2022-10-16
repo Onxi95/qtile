2705ed1d71a5912484c0da94426d099864bfc7b5
Paweł Sowa <pawel@the-sowa.com>
2022-10-16 14:08:33 +0200

    chore: add correct display mode

--- autostart.sh
+++ autostart.sh
@@ -10,7 +10,7 @@ nm-applet &
 
 xrandr --newmode "5120x1440_55.00"  568.25  5120 5480 6032 6944  1440 1443 1453 1489 -hsync +vsync
 xrandr --addmode DP-2 5120x1440_55.00
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 5120x236 --rotate normal --output DP-1 --off --output DP-2 --mode 5120x1440_55.00 --pos 0x0 --rotate normal --output DP-3 --off
+xrandr --output eDP-1 --mode 1920x1080 --pos 5120x236 --rotate normal --output DP-1 --off --output DP-2 --primary --mode 5120x1440_55.00 --pos 0x0 --rotate normal --output DP-3 --off
 # xrandr --output eDP-1 --mode 1920x1080 --pos 3840x0 --rotate normal --output DP-1 --off --output DP-2 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-3 --off --output DP-1-1 --off --output DP-1-2 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1-3 --off
 ### UNCOMMENT ONLY ONE OF THE FOLLOWING THREE OPTIONS! ###
 # 1. Uncomment to restore last saved wallpaper
