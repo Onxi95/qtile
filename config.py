1e6ada4e78d1f4262b219aff197ea15c40adeb74
Paweł Sowa <pawel@the-sowa.com>
2023-06-30 19:02:26 +0200

    chore: autopep8 format on save

--- config.py
+++ config.py
@@ -9,45 +9,77 @@ from libqtile.command import lazy
 from libqtile import layout, bar, widget, hook
 from libqtile.utils import guess_terminal
 
-mod = "mod4"               # Sets mod key to SUPER/WINDOWS
-myTerm = guess_terminal()  # My terminal of choice
-myBrowser = "brave"        # My browser of choice
+# Sets mod key to SUPER/WINDOWS
+mod = "mod4"
+# My terminal of choice
+myTerm = guess_terminal()
+# My browser of choice
+myBrowser = "brave"
 
 defaultFont = "RobotoMono Nerd Font"
 defaultFontSize = 14
 
 keys = [
     # The essentials
-    Key([mod], "Return", lazy.spawn(myTerm)),
-    Key([mod, "shift"], "Return", lazy.spawn("rofi -show drun")),
-    Key([mod, "shift"], "f", lazy.spawn("flameshot gui")),
-    Key([mod], "Escape", lazy.spawn("betterlockscreen -l")),
-    Key([mod], "b", lazy.spawn(myBrowser)),
-    Key([mod], "Tab", lazy.next_layout()),
-    Key([mod, "shift"], "c", lazy.window.kill()),
-    Key([mod, "shift"], "r", lazy.restart()),
-    Key([mod, "shift"], "q", lazy.shutdown()),
-    Key([mod, "shift"], "s", lazy.spawn("shutdown now")),
+    Key([mod], "Return",
+        lazy.spawn(myTerm)),
+    Key([mod, "shift"], "Return",
+        lazy.spawn("rofi -show drun")),
+    Key([mod, "shift"], "f", lazy.spawn(
+        "flameshot gui")),
+    Key([mod], "Escape", lazy.spawn(
+        "betterlockscreen -l")),
+    Key([mod], "b", lazy.spawn(
+        myBrowser)),
+    Key([mod], "Tab",
+        lazy.next_layout()),
+    Key([mod, "shift"], "c",
+        lazy.window.kill()),
+    Key([mod, "shift"],
+        "r", lazy.restart()),
+    Key([mod, "shift"],
+        "q", lazy.shutdown()),
+    Key([mod, "shift"], "s",
+        lazy.spawn("shutdown now")),
     # Switch focus of monitors
-    Key([mod], "period", lazy.next_screen()),
-    Key([mod], "comma", lazy.prev_screen()),
-    Key([mod], "m", lazy.layout.maximize(),),
-    Key([mod], "f", lazy.window.toggle_floating(),),
-    Key([mod], "j", lazy.layout.down()),
-    Key([mod], "k", lazy.layout.up()),
-    Key([mod], "h", lazy.layout.left()),
-    Key([mod], "l", lazy.layout.right()),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down()),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up()),
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left()),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right()),
-    Key([mod, "control"], "j", lazy.layout.grow_down()),
-    Key([mod, "control"], "k", lazy.layout.grow_up()),
-    Key([mod, "control"], "h", lazy.layout.grow_left()),
-    Key([mod, "control"], "l", lazy.layout.grow_right()),
-    Key([mod, "shift", "control"], "h", lazy.layout.swap_column_left()),
-    Key([mod, "shift", "control"], "l", lazy.layout.swap_column_right()),
-    Key([mod], "n", lazy.layout.normalize()),
+    Key([mod], "period",
+        lazy.next_screen()),
+    Key([mod], "comma",
+        lazy.prev_screen()),
+    Key([mod], "m",
+        lazy.layout.maximize(),),
+    Key([mod], "f",
+        lazy.window.toggle_floating(),),
+    Key([mod], "j",
+        lazy.layout.down()),
+    Key([mod], "k",
+        lazy.layout.up()),
+    Key([mod], "h",
+        lazy.layout.left()),
+    Key([mod], "l",
+        lazy.layout.right()),
+    Key([mod, "shift"], "j",
+        lazy.layout.shuffle_down()),
+    Key([mod, "shift"], "k",
+        lazy.layout.shuffle_up()),
+    Key([mod, "shift"], "h",
+        lazy.layout.shuffle_left()),
+    Key([mod, "shift"], "l",
+        lazy.layout.shuffle_right()),
+    Key([mod, "control"], "j",
+        lazy.layout.grow_down()),
+    Key([mod, "control"], "k",
+        lazy.layout.grow_up()),
+    Key([mod, "control"], "h",
+        lazy.layout.grow_left()),
+    Key([mod, "control"], "l",
+        lazy.layout.grow_right()),
+    Key([mod, "shift", "control"], "h",
+        lazy.layout.swap_column_left()),
+    Key([mod, "shift", "control"], "l",
+        lazy.layout.swap_column_right()),
+    Key([mod], "n",
+        lazy.layout.normalize()),
     # Stack controls
     Key([mod, "shift"], "Tab",
         lazy.layout.rotate(),
@@ -65,16 +97,21 @@ keys = [
 ]
 
 groups = [Group("1", layout='columns'),
-          Group("2", layout='columns'),
-          Group("3", layout='columns'),
-          Group("4", layout='columns'),
-          Group("5", layout='columns'),
+          Group(
+              "2", layout='columns'),
+          Group(
+              "3", layout='columns'),
+          Group(
+              "4", layout='columns'),
+          Group(
+              "5", layout='columns'),
           ]
 
 # Allow MODKEY+[0 through 9] to bind to groups, see https://docs.qtile.org/en/stable/manual/config/groups.html
 # MOD4 + index Number : Switch to Group[index]
 # MOD4 + shift + index Number : Send active window to another Group
-dgroups_key_binder = simple_key_binder("mod4")
+dgroups_key_binder = simple_key_binder(
+    "mod4")
 
 layout_theme = {"border_width": 2,
                 "margin": 8,
@@ -83,9 +120,12 @@ layout_theme = {"border_width": 2,
                 }
 
 layouts = [
-    layout.Columns(**layout_theme, num_columns=3),
-    layout.RatioTile(**layout_theme),
-    layout.MonadThreeCol(**layout_theme),
+    layout.Columns(
+        **layout_theme, num_columns=3),
+    layout.RatioTile(
+        **layout_theme),
+    layout.MonadThreeCol(
+        **layout_theme),
 ]
 
 colors = {
@@ -130,7 +170,8 @@ def init_widgets_list():
             fontsize=defaultFontSize
         ),
         widget.CurrentLayoutIcon(
-            custom_icon_paths=[os.path.expanduser("~/.config/qtile/icons")],
+            custom_icon_paths=[os.path.expanduser(
+                "~/.config/qtile/icons")],
             foreground=colors["gray"],
             padding=0,
             scale=0.7
@@ -153,23 +194,30 @@ def init_widgets_list():
     ]
 
     right_side_widgets = [
-        partial(widget.Net, interface="wlan0", format='Net: {down} ↓↑ {up}'),
+        partial(widget.Net, interface="wlan0",
+                format='Net: {down} ↓↑ {up}'),
         partial(widget.Memory, mouse_callbacks={
                 'Button1': lambda: qtile.cmd_spawn(myTerm + ' -e htop')}, fmt='Mem: {}'),
-        partial(widget.Volume, fmt='Vol: {}'),
+        partial(
+            widget.Volume, fmt='Vol: {}'),
         partial(widget.Volume, device="Capture", fmt='Mic: {}',
-                get_volume_command="amixer get Capture".split(" "),
+                get_volume_command="amixer get Capture".split(
+                    " "),
                 volume_down_command="amixer set Capture 2%-",
                 volume_up_command="amixer set Capture 2%+",
                 mute_command="amixer set Capture toggle"),
         partial(widget.KeyboardLayout, fmt='Keyboard: {}',
                 configured_keyboards=['pl', 'us']),
-        partial(widget.Battery),
-        partial(widget.CryptoTicker, update_interval=30),
-        partial(widget.CryptoTicker, crypto="ETH", update_interval=30),
+        partial(
+            widget.Battery),
+        partial(widget.CryptoTicker,
+                update_interval=30),
+        partial(widget.CryptoTicker,
+                crypto="ETH", update_interval=30),
         partial(widget.CryptoTicker, crypto="USDT",
                 currency="PLN", update_interval=30),
-        partial(widget.Clock, format="%A, %B %d - %H:%M "),
+        partial(
+            widget.Clock, format="%A, %B %d - %H:%M "),
     ]
 
     m_right_side_widgets = [f(padding=10) if index % 2 == 0 else f(padding=10, background=colors["blue"])
@@ -184,20 +232,23 @@ if __name__ in ["config", "__main__"]:
             bottom=bar.Bar(widgets=init_widgets_list(),
                            opacity=1.0,
                            size=25,
-                           background=[colors["background"]]
+                           background=[
+                               colors["background"]]
                            )),
         Screen(
             bottom=bar.Bar(
                 widgets=init_widgets_list(),
                 opacity=1.0,
                 size=25,
-                background=[colors["background"]]
+                background=[
+                    colors["background"]]
             )),
         Screen(
             bottom=bar.Bar(widgets=init_widgets_list(),
                            opacity=1.0,
                            size=25,
-                           background=[colors["background"]]
+                           background=[
+                               colors["background"]]
                            ))]
     widgets_list = init_widgets_list()
 
@@ -207,7 +258,8 @@ mouse = [
          start=lazy.window.get_position()),
     Drag([mod], "Button3", lazy.window.set_size_floating(),
          start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.bring_to_front())
+    Click([mod], "Button2",
+          lazy.window.bring_to_front())
 
 
 ]
@@ -217,25 +269,36 @@ floating_layout = layout.Floating(float_rules=[
     # default_float_rules include: utility, notification, toolbar, splash, dialog,
     # file_progress, confirm, download and error.
     *layout.Floating.default_float_rules,
-    Match(title='Confirmation'),      # tastyworks exit box
-    Match(title='Qalculate!'),        # qalculate-gtk
-    Match(wm_class='kdenlive'),       # kdenlive
-    Match(wm_class='pinentry-gtk-2'),  # GPG key password entry
+    # tastyworks exit box
+    Match(
+        title='Confirmation'),
+    # qalculate-gtk
+    Match(title='Qalculate!'),
+    # kdenlive
+    Match(
+        wm_class='kdenlive'),
+    # GPG key password entry
+    Match(
+        wm_class='pinentry-gtk-2'),
 ])
 
 
 @hook.subscribe.startup_once
 def start_once():
-    home = os.path.expanduser('~')
-    subprocess.call([home + '/.config/qtile/autostart.sh'])
+    home = os.path.expanduser(
+        '~')
+    subprocess.call(
+        [home + '/.config/qtile/autostart.sh'])
 
 
 @hook.subscribe.startup_complete
 def assign_groups_to_screens():
-    screen_arrangement = [0, 1]
+    screen_arrangement = [
+        0, 1]
 
     for index, screen_number in enumerate(screen_arrangement):
-        qtile.groups_map[groups[index].name].cmd_toscreen(screen_number)
+        qtile.groups_map[groups[index].name].cmd_toscreen(
+            screen_number)
 
 
 # XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
